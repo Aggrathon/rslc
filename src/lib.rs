af067770ca60530aac00381da72e404453d26390
Aggrathon <antonbjo@gmail.com>
2020-09-07 00:14:11 +0300

    flood fill

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,9 @@
-use ndarray::{ArrayBase, Data, Ix2, Ix1, s, DataOwned, DataMut};
+use ndarray::{ArrayBase, Data, Ix2, Ix1, s, DataOwned, DataMut, Array1};
 use numpy::{IntoPyArray, PyArray2};
 use pyo3::exceptions::RuntimeError;
 use pyo3::prelude::{pymodule, Py, PyErr, PyModule, PyResult, Python};
 use std::fmt::Display;
+use std::collections::VecDeque;
 
 
 fn make_error<E: Display + Sized>(e: E) -> PyErr {
@@ -26,7 +27,7 @@ const fn _linear_index(i: usize, j: usize, w: usize) -> usize {
 
 fn matrix_index(k: usize, w: usize) -> (usize, usize) {
     assert!(k < w * (w - 1) / 2);
-    let mut j = k / w;
+    let mut j = k / (w-1);
     while _linear_index(j+2, j+1, w) <= k { j += 1; }
     (k - _linear_index(j+1, j, w) + j + 1, j)
 }
@@ -52,6 +53,67 @@ fn flat_distance<A, S1, S2>(matrix: &ArrayBase<S1, Ix2>) -> ArrayBase<S2, Ix1> w
     }
     flat
 }
+
+struct FloodFill<'a, S> where S: Data<Elem = usize> {
+    start: usize,
+    current: usize,
+    cache: VecDeque<usize>,
+    clusters: Array1<usize>,
+    sizes: Vec<(usize, usize)>,
+    distances: &'a ArrayBase<S, Ix2>,
+
+}
+
+impl<'a, S> FloodFill<'a, S> where S: Data<Elem = usize> {
+    fn new(distances: &'a ArrayBase<S, Ix2>) -> Self {
+        FloodFill {
+            start: 0,
+            current: 0,
+            cache: VecDeque::with_capacity(distances.ncols()),
+            clusters: Array1::zeros(distances.ncols()),
+            sizes: vec!(),
+            distances
+        }
+    }
+
+    fn flood_fill(self: &mut Self) {
+        self.start = self.current + 1;
+        let mut current = self.start;
+        self.sizes.clear();
+        while let Some((i, _)) = self.clusters.iter().enumerate().find(|(i, x)| **x < self.start) {
+            self.cache.clear();
+            self.clusters[i] = current;
+            self.cache.push_back(i);
+            let mut count = 1;
+            while let Some(i) = self.cache.pop_front() {
+                for (j, _) in self.distances.outer_iter().nth(i).unwrap().iter().enumerate().filter(|(_, y)| **y < current) {
+                    self.clusters[j] = self.current;
+                    self.cache.push_back(j);
+                    count += 1;
+                }
+            }
+            self.sizes.push((current, count));
+            current += 1;
+        }
+        self.current = current;
+    }
+}
+
+fn partial_flood_fill<S1, S2>(dists: &ArrayBase<S1, Ix2>, start: usize, current: usize, clusters: &mut ArrayBase<S2, Ix1>) -> usize where S1: Data, S2: DataMut<Elem = usize> {
+    match clusters.iter().enumerate().find(|(i, x)| **x < start){
+        Some((i, _)) => {
+            let mut i = i;
+            let mut min = i;
+            loop {
+
+            }
+            0
+        },
+        None => 0
+    }
+}
+
+
 
 #[pymodule]
 fn rust_linalg(_py: Python<'_>, m: &PyModule) -> PyResult<()> {
