c385f7f483b4bac650c066d6cfcba00cacfe64fb
Aggrathon <antonbjo@gmail.com>
2020-09-20 21:29:10 +0300

    Empirical evaluation of the complexity

--- python/rslc/__init__.py
+++ python/rslc/__init__.py
@@ -1,9 +1,11 @@
-
+from typing import Tuple
 import numpy as np
-from .rslc import *
+from . import rslc as _rslc
 
 
-def cluster(distance_matrix: np.ndarray, num_clusters: int, min_size: int = None) -> (np.ndarray, np.ndarray):
+def cluster(
+    distance_matrix: np.ndarray, num_clusters: int, min_size: float = None
+) -> Tuple[np.ndarray, np.ndarray]:
     """The Robust Single-Linkage Clustering Algorithm
 
     Args:
@@ -17,7 +19,7 @@ def cluster(distance_matrix: np.ndarray, num_clusters: int, min_size: int = None
     """
     # Default value for min_size
     if min_size is None:
-        min_size = num_clusters**-2
+        min_size = num_clusters ** -2
     # Some sanity checks
     assert len(distance_matrix.shape) == 2
     assert distance_matrix.shape[0] == distance_matrix.shape[1]
@@ -28,19 +30,19 @@ def cluster(distance_matrix: np.ndarray, num_clusters: int, min_size: int = None
         min_size = int(distance_matrix.shape[0] * min_size)
     # Select correct type and run the algorithm
     if distance_matrix.dtype == np.float64:
-        return rslc_f64(distance_matrix, num_clusters, min_size)
+        return _rslc.rslc_f64(distance_matrix, num_clusters, min_size)
     if distance_matrix.dtype == np.float32:
-        return rslc_f32(distance_matrix, num_clusters, min_size)
+        return _rslc.rslc_f32(distance_matrix, num_clusters, min_size)
     if distance_matrix.dtype == np.int32:
-        return rslc_i32(distance_matrix, num_clusters, min_size)
+        return _rslc.rslc_i32(distance_matrix, num_clusters, min_size)
     if distance_matrix.dtype == np.int64:
-        return rslc_i64(distance_matrix, num_clusters, min_size)
+        return _rslc.rslc_i64(distance_matrix, num_clusters, min_size)
     if distance_matrix.dtype == np.uint32:
-        return rslc_u32(distance_matrix, num_clusters, min_size)
+        return _rslc.rslc_u32(distance_matrix, num_clusters, min_size)
     if distance_matrix.dtype == np.uint64:
-        return rslc_u64(distance_matrix, num_clusters, min_size)
+        return _rslc.rslc_u64(distance_matrix, num_clusters, min_size)
     if distance_matrix.dtype == np.int16:
-        return rslc_i16(distance_matrix, num_clusters, min_size)
+        return _rslc.rslc_i16(distance_matrix, num_clusters, min_size)
     if distance_matrix.dtype == np.uint16:
-        return rslc_u16(distance_matrix, num_clusters, min_size)
-    return rslc_f64(distance_matrix.astype(np.float32), num_clusters, min_size)
+        return _rslc.rslc_u16(distance_matrix, num_clusters, min_size)
+    return _rslc.rslc_f64(distance_matrix.astype(np.float64), num_clusters, min_size)
